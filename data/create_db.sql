9371faacbbfda116c88a0b500deeda1ea91d2483
yixiao17 <yixiao17@vt.edu>
2022-12-02 19:00:55 -0500

    content fix

--- data/create_db.sql
+++ data/create_db.sql
@@ -1,3 +1,14 @@
+Drop table course_non_students;
+Drop table course_students;
+Drop table Course;
+Drop table Non_diploma_student;
+Drop table Student;
+Drop table Major;
+Drop table School;
+Drop table Professor;
+Drop table Student_Work;
+Drop table Degree;
+
 create table Degree(
         level varchar(32) not null,
         type varchar(32) not null,
@@ -70,7 +81,7 @@ create table course_students(
         course_number integer not null,
         section varchar(32) not null,
         term varchar(32) not null,
-        grade interger,
+        grade integer,
         primary key (student_ssn, course_number, section, term),
         foreign key (student_ssn) references Student(ssn),
         foreign key (course_number, section, term) references Course(course_number, section, term)
@@ -81,9 +92,58 @@ create table course_non_students(
         course_number integer not null,
         section varchar(32) not null,
         term varchar(32) not null,
-        grade interger,
+        grade integer,
         primary key (student_ssn, course_number, section, term),
         foreign key (student_ssn) references Non_diploma_student(ssn),
         foreign key (course_number, section, term) references Course(course_number, section, term)
 );
 
+COPY Degree
+FROM 'D:\DBProject\data\csv\Degree.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Student_Work
+FROM 'D:\DBProject\data\csv\Student_Work.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Professor
+FROM 'D:\DBProject\data\csv\Professor.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY School
+FROM 'D:\DBProject\data\csv\School.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Major
+FROM 'D:\DBProject\data\csv\Major.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Student
+FROM 'D:\DBProject\data\csv\Student.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Non_diploma_student
+FROM 'D:\DBProject\data\csv\Non_diploma_student.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Course
+FROM 'D:\DBProject\data\csv\Course.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY course_students
+FROM 'D:\DBProject\data\csv\course_students.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY course_non_students
+FROM 'D:\DBProject\data\csv\course_non_students.csv'
+DELIMITER ','
+CSV HEADER;
